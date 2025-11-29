b7484e2dec5db69df8be002f50330eefdf0834b6
Diego Alejandro Flores Quintero <diegoa.floresq@uqvirtual.edu.co>
2025-11-28 23:58:53 -0500

    feat: enhance security and CORS configuration for traceability microservice
    - Added JWT secret key to docker-compose.yml for secure token generation.
    - Updated JwtProvider to use UTF-8 encoding for the secret key.
    - Modified application.yml to reference the JWT secret from the environment variable.

--- src/main/java/com/pragma/powerup/traceabilitymicroservice/infrastructure/security/JwtProvider.java
+++ src/main/java/com/pragma/powerup/traceabilitymicroservice/infrastructure/security/JwtProvider.java
@@ -2,12 +2,12 @@ package com.pragma.powerup.traceabilitymicroservice.infrastructure.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.function.Function;
 
@@ -71,7 +71,6 @@ public class JwtProvider {
     }
 
     private SecretKey getKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
-        return Keys.hmacShaKeyFor(keyBytes);
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 }
